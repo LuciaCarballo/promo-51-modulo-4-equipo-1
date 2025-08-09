9dc9e6a1321a14ce384f0be3be578778ec639ab6
LuciaCarballo <lulucarcas98@gmail.com>
2025-08-09 20:22:47 +0200

    servidor estatico

--- src/web/vite.config.js
+++ src/web/vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/promo-51-modulo-3-equipo-1/",
-  plugins: [react()],
+    plugins: [react()],
 
   server: {
   open: "/",
