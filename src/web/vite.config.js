54372ed8618ed9b83742ec7b3f53f6f57b5fe93b
LuciaCarballo <lulucarcas98@gmail.com>
2025-08-11 22:15:48 +0200

    Configura despliegue en GitHub Pages

--- src/web/vite.config.js
+++ src/web/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-   // base: "/promo-51-modulo-4-equipo-1/",
+    base: "/promo-51-modulo-4-equipo-1/",
     plugins: [react()],
 
   server: {
